6c88c9f8667de6e31f71d6b6b4c3e9bb2b4ca90d
neuromemeser <necromemeser@gmail.com>
2026-01-30 17:23:41 +0300

    first commit

---
+++ pkg/service/auth.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"crypto/sha1"
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	todo "main.go"
+	"main.go/pkg/repository"
+)
+
+const (
+	salt       = "sgkcjvfgjhfjgha35"
+	signingKey = "sfjshfdjnjgh42ljh2sfjsdfjA"
+	tokenTTL   = 12 * time.Hour
+)
+
+type TokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
+
+type AuthService struct {
+	repo repository.Authorization
+}
+
+func NewAuthService(repo repository.Authorization) *AuthService {
+	return &AuthService{repo: repo}
+}
+
+func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = s.generatePasswordHash(user.Password)
+	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, s.generatePasswordHash(password))
+	if err != nil {
+		return "", nil
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &TokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
+
+}
+
+func (s *AuthService) generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
+}
