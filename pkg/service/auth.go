fa45e0fc66738a13ed6b4149a3f331c101f7c866
neuromemeser <necromemeser@gmail.com>
2026-01-31 15:01:35 +0300

    second commit

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
 	"time"
 
@@ -16,7 +17,7 @@ const (
 	tokenTTL   = 12 * time.Hour
 )
 
-type TokenClaims struct {
+type tokenClaims struct {
 	jwt.StandardClaims
 	UserId int `json:"user_id"`
 }
@@ -40,7 +41,7 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 		return "", nil
 	}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &TokenClaims{
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
 		jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
 			IssuedAt:  time.Now().Unix(),
@@ -50,6 +51,26 @@ func (s *AuthService) GenerateToken(username, password string) (string, error) {
 
 	return token.SignedString([]byte(signingKey))
 
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(signingKey), nil
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func (s *AuthService) generatePasswordHash(password string) string {
