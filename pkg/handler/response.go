58738802586e351bb8632d6b4cee278dc1a1bce4
Vladimir Dorofeev <necromemeser@gmail.com>
2026-02-04 18:06:45 +0300

    implementing crud

--- pkg/handler/response.go
+++ pkg/handler/response.go
@@ -5,11 +5,15 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type error struct {
+type errorResponse struct {
 	Message string `json:"message"`
+}
+
+type statusResposnse struct {
+	Status string `json:"status"`
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
 	logrus.Error(message)
-	c.AbortWithStatusJSON(statusCode, error{message})
+	c.AbortWithStatusJSON(statusCode, errorResponse{message})
 }
